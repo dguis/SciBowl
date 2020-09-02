958fe1a36888cd897d38a67bd3c2f679a3520f55
dguis <dallin.guisti@gmail.com>
2020-09-02 08:24:53 -0700

    Update test file

--- test.py
+++ test.py
@@ -1,1 +1,10 @@
-"" 
+import main as ctt   # your code file to test (rename as needed)
+
+import unittest   # The test framework
+
+class Test_TTT(unittest.TestCase):
+    def test_1(self): #can rename a test function to whatever you like
+        print("testing...")
+
+if __name__ == '__main__':
+    unittest.main()
